0ee45bc3ee3b06a88398fcbcd7e8bad55af7e075
ebennatti <ebennatti@gmail.com>
2026-08-30 21:03:00 -0300

    bibli_03 + biblioteca estatica

---
+++ 03_bibliotecas/bibli_03/Resultados/Eduardo/completo/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "data.h"
+
+int main() {
+    int dia1, mes1, ano1;
+    int dia2, mes2, ano2;
+
+    scanf("%d/%d/%d", &dia1, &mes1, &ano1);
+    scanf("%d/%d/%d", &dia2, &mes2, &ano2);
+
+    if(!verificaDataValida(dia1, mes1, ano1) || !verificaDataValida(dia2, mes2, ano2))
+        printf("A primeira e/ou segunda data(s) invalida(s)\n");
+    else {
+        //imprime por extenso 
+        printf("Primeira data: ");
+        imprimeDataExtenso(dia1, mes1, ano1);
+        printf("Segunda data: ");
+        imprimeDataExtenso(dia2, mes2, ano2);
+
+        //verifica qual data eh mais antiga
+        if(comparaData(dia1, mes1, ano1, dia2, mes2, ano2) == -1) {
+            printf("A primeira data eh mais antiga\n");
+        } else if(comparaData(dia1, mes1, ano1, dia2, mes2, ano2) == 1) {
+            printf("A segunda data eh mais antiga\n");
+        } else 
+            printf("As datas sao iguais\n");
+
+        //imprime diferenca de dias
+        printf("A diferenca em dias entre as datas eh: %02d dias\n", calculaDiferencaDias(dia1, mes1, ano1, dia2, mes2, ano2));
+    }
+
+    return 0;
+}
